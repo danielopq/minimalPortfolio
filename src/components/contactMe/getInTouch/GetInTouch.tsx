0bf66c1807dd8975f103f1fff998f8e513c6a58e
Daniel Martinez Duque <danielopq@gmail.com>
2024-10-19 16:39:58 +0100

    GetInTouch.tsx - created

--- src/components/contactMe/getInTouch/GetInTouch.tsx
+++ src/components/contactMe/getInTouch/GetInTouch.tsx
@@ -1,9 +1,22 @@
 import './getInTouch.css';
 
-const GetInTouch:React.FC = ()=>{
-    return(
-        <section id="contactMe-getInTouch">
-
+const GetInTouch: React.FC = () => {
+    return (
+        <section id="getInTouch">
+            <h2>Get in Touch</h2>
+            <div id="getInTouch-content">
+                <p className='mainText'>
+                    I’d love to hear about what you’re working on and how I
+                    could help. I’m currently looking for a new role and am
+                    open to a wide range of opportunities. My preference would
+                    be to find a position in a company in London. But I’m also
+                    happy to hear about opportunites that don’t fit that
+                    description. I’m a hard-working and positive person who will
+                    always approach each task with a sense of purpose and attention
+                    to detail. Please do feel free to check out my online profiles
+                    below and get in touch using the form.
+                </p>
+            </div>
         </section>
     )
 }
