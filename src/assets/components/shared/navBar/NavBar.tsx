7363cfd57b139f61beab84e666a99ab2e1ae5a86
Daniel Martinez Duque <danielopq@gmail.com>
2024-10-19 04:47:04 +0100

    NavBar.tsx - done

--- src/assets/components/shared/navBar/NavBar.tsx
+++ src/assets/components/shared/navBar/NavBar.tsx
@@ -1,8 +1,30 @@
+import { useState } from 'react';
 import './navBar.css';
 
-const Navbar:React.FC = ()=>{
-    return(
-        <nav id="navaBar"></nav>
+/**
+ * Renders the main navigation bar of the website.
+ * @returns {JSX.Element} The main navigation bar component.
+ */
+const Navbar: React.FC = () => {
+
+    const [currentSection,setCurrentSection] = useState<string>('home');
+
+    /**
+     * Updates the current section state to the selected section.
+     * @param {string} section - The section to navigate to.
+     */
+    const changeSection = (section:string)=>{
+        setCurrentSection(section);
+    }
+
+    return (
+        <nav id="navBar">
+            <div id="navBarLinks">
+                <button onClick={()=>changeSection('home')} className={currentSection === 'home' ? 'navBarActiveLink' : 'navBarInactiveLink'}>HOME</button>
+                <button onClick={()=>changeSection('portfolio')} className={currentSection === 'portfolio' ? 'navBarActiveLink' : 'navBarInactiveLink'}>PORTFOLIO</button>
+                <button onClick={()=>changeSection('contact')} className={currentSection === 'contact' ? 'navBarActiveLink' : 'navBarInactiveLink'}>CONTACT ME</button>
+            </div>
+        </nav>
     )
 }
 export default Navbar;
