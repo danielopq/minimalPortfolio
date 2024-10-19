682c03e62deb1f5db5353b580395bb8b6b4c1f9c
Daniel Martinez Duque <danielopq@gmail.com>
2024-10-19 13:36:47 +0100

    AboutMe.tsx - created

--- src/assets/components/home/Home.tsx
+++ src/assets/components/home/Home.tsx
@@ -1,10 +1,12 @@
 import './home.css';
 import HomeHeader from './homeHeader/HomeHeader';
+import AboutMe from './aboutMe/AboutMe';
 
 const Home:React.FC = () =>{
     return(
         <main id="home">
             <HomeHeader/>
+            <AboutMe/>
         </main>
     )
 }
---
+++ src/assets/components/home/aboutMe/AboutMe.tsx
@@ -0,0 +1,34 @@
+import './aboutMe.css';
+
+const AboutMe: React.FC = () => {
+    return (
+        <section id="aboutMe">
+            <div id="aboutMe-topSide">
+                <div id="aboutMe-mainPic"></div>
+                <div id="aboutMe-mainContent">
+                    <h2>About Me</h2>
+                    <p className='mainText'>
+                        I’m a junior front-end developer
+                        looking for a new role in an exciting company. I
+                        focus on writing accessible HTML, using modern CSS
+                        practices and writing clean JavaScript. When writing
+                        JavaScript code, I mostly use React, but I can adapt
+                        to whatever tools are required. I’m based in London,
+                        UK, but I’m happy working remotely and have experience
+                        in remote teams. When I’m not coding, you’ll find me
+                        outdoors. I love being out in nature whether that’s
+                        going for a walk, run or cycling. I’d love you to check
+                        out my work.
+                    </p>
+                    <button>GO TO PORTFOLIO</button>
+                </div>
+            </div>
+            <div id="aboutMe-bottomSide">
+                <h2>Interested in doing a project together?</h2>
+                <div id="aboutMe-line"></div>
+                <button>CONTACT ME</button>
+            </div>
+        </section>
+    )
+}
+export default AboutMe;
