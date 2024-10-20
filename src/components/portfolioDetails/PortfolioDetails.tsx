276fceca3f29f6424112b4aac76cca9ab35f807b
Daniel Martinez Duque <danielopq@gmail.com>
2024-10-20 19:33:25 +0100

    DetailsNavBar.tsx - created

--- src/components/portfolioDetails/PortfolioDetails.tsx
+++ src/components/portfolioDetails/PortfolioDetails.tsx
@@ -1,10 +1,11 @@
 import './portfolioDetails.css';
-
 import ContactFooter from '../shared/contactFooter/ContactFooter';
+import DetailsNavBar from './detailsNavBar/DetailsNavBar';
 
 const PortfolioDetails:React.FC = ()=>{
     return(
         <main id="portfolioDetails">
+            <DetailsNavBar/>
             <ContactFooter/>
         </main>
     )
---
+++ src/components/portfolioDetails/detailsNavBar/DetailsNavBar.tsx
@@ -0,0 +1,10 @@
+import './detailsNavBar.css';
+
+const DetailsNavBar:React.FC = () =>{
+    return(
+        <nav id="detailsNavBar">
+
+        </nav>
+    )
+}
+export default DetailsNavBar;
