63ce4cc0abb3f0bf69874f534f7180354182d116
Daniel Martinez Duque <danielopq@gmail.com>
2024-10-20 14:19:38 +0100

    PortfolioOverview.tsx - main layout done

--- src/components/portfolio/portfolioIndex/PortfolioIndex.tsx
+++ src/components/portfolio/portfolioIndex/PortfolioIndex.tsx
@@ -1,9 +1,12 @@
 import './portfolioIndex.css';
+import PortfolioOverview from '../portfolioOverview/PortfolioOverview';
+
 
 const PortfolioIndex:React.FC = () =>{
     return(
         <section id="portfolioIndex">
-
+            <PortfolioOverview />
+            <PortfolioOverview />
         </section>
     )
 }
--- src/components/portfolio/portfolioOverview/PortfolioOverview.tsx
+++ src/components/portfolio/portfolioOverview/PortfolioOverview.tsx
@@ -3,17 +3,19 @@ import StandardButton from '../../shared/standardButton/StandardButton';
 
 const PortfolioOverview: React.FC = () => {
     return (
-        <section className="overview">
+        <section className="overview rightSide">
             <div className="overview-mainPic"></div>
             <div className="overview-mainContent">
-                <h2>Manage</h2>
-                <p className='mainText'>
-                    This project required me to build a fully responsive
-                    landing page to the designs provided. I used HTML5,
-                    along with CSS Grid and JavaScript for the areas that
-                    required interactivity, such as the testimonial slider.
-                </p>
-                <StandardButton value='VIEW PROJECT' mode='lightButton' />
+                <div>
+                    <h2>Manage</h2>
+                    <p className='mainText'>
+                        This project required me to build a fully responsive
+                        landing page to the designs provided. I used HTML5,
+                        along with CSS Grid and JavaScript for the areas that
+                        required interactivity, such as the testimonial slider.
+                    </p>
+                    <StandardButton value='VIEW PROJECT' mode='lightButton' />
+                </div>
             </div>
         </section>
     )
