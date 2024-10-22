1931396586e85def8d3c1f4a548ac773fd9b5842
Daniel Martinez Duque <danielopq@gmail.com>
2024-10-22 13:36:45 +0100

    ContactForm.tsx - finished

--- src/components/contactMe/contactForm/ContactForm.tsx
+++ src/components/contactMe/contactForm/ContactForm.tsx
@@ -16,67 +16,109 @@ const ContactForm: React.FC = () => {
     const refEmailError = useRef<HTMLParagraphElement>(null);
     const refMessageError = useRef<HTMLParagraphElement>(null);
 
-    const isValidEmail=(email:string):boolean=> {
+    const refConfirmationWindow = useRef<HTMLDivElement>(null);
+
+    /**
+     * Checks whether the email format is valid.
+     * @param {string} email - The email address to be validated.
+     * @returns {boolean} - Returns true if the email format is valid, otherwise false.
+     */
+    const isValidEmail = (email: string): boolean => {
         const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         return emailPattern.test(email);
     }
 
-    const sendForm = ():void=>{
-        if(refName.current && refNameError.current){
-            if(refName.current.value.trim() == ''){
+    /**
+     * Validates the form fields and prevents form submission if validation fails.
+     * Displays a confirmation window if all fields are valid.
+     * @param {React.FormEvent<HTMLFormElement>} event - The form submission event.
+     */
+    const sendForm = (event: React.FormEvent<HTMLFormElement>): void => {
+        let validForm = true;
+        event.preventDefault();
+
+        if (refName.current && refNameError.current) {
+            if (refName.current.value.trim() == '') {
                 refName.current.className = 'formatError';
                 refNameError.current.innerHTML = 'This field is required';
-            }else{
+                validForm = false;
+            } else {
                 refName.current.className = '';
                 refNameError.current.innerHTML = '';
             }
         }
 
-        if(refEmail.current && refEmailError.current){
-            if(refEmail.current.value.trim() == ''){
+        if (refEmail.current && refEmailError.current) {
+            if (refEmail.current.value.trim() == '') {
                 refEmail.current.className = 'formatError';
                 refEmailError.current.innerHTML = 'This field is required';
-            }else if(!isValidEmail(refEmail.current.value)){
+                validForm = false;
+            } else if (!isValidEmail(refEmail.current.value)) {
                 refEmail.current.className = 'formatError';
                 refEmailError.current.innerHTML = 'Invalid email format';
+                validForm = false;
             }
-            else{
+            else {
                 refEmail.current.className = '';
                 refEmailError.current.innerHTML = '';
             }
         }
-        
-        if(refMessage.current && refMessageError.current){
-            if(refMessage.current.value.trim() == ''){
+
+        if (refMessage.current && refMessageError.current) {
+            if (refMessage.current.value.trim() == '') {
                 refMessage.current.className = 'formatError';
                 refMessageError.current.innerHTML = 'This field is required';
-            }else{
+                validForm = false;
+            } else {
                 refMessage.current.className = '';
                 refMessageError.current.innerHTML = '';
             }
         }
+
+        if(refConfirmationWindow.current && validForm){
+            refConfirmationWindow.current.setAttribute('style','display:flex');
+            refName.current && (refName.current.value = '');
+            refEmail.current && (refEmail.current.value = '');
+            refMessage.current && (refMessage.current.value = '');
+        }
+    }
+
+    /**
+     * Hides the confirmation window after the form has been submitted successfully.
+     */
+    const hideConfirmationWindow = ():void =>{
+        refConfirmationWindow.current && refConfirmationWindow.current.setAttribute('style','display:none');
     }
 
     return (
         <section id="contactForm">
             <h2>Contact Me</h2>
-            <div id="contactForm-content">
-                <div className='formElement'>
-                    <label htmlFor="name">Name</label>
-                    <input type="text" ref={refName} id="name" name="name" required placeholder='Your name'/>
-                    <p ref={refNameError} className='formErrorMsg'></p>
+            <form onSubmit={sendForm}>
+                <div id="contactForm-content">
+                    <div className='formElement'>
+                        <label htmlFor="name">Name</label>
+                        <input type="text" ref={refName} id="name" name="name" placeholder='Your name' />
+                        <p ref={refNameError} className='formErrorMsg'></p>
+                    </div>
+                    <div className='formElement'>
+                        <label htmlFor="email">Email Address</label>
+                        <input type="email" ref={refEmail} id="email" name="email" placeholder='email@example.com' />
+                        <p ref={refEmailError} className='formErrorMsg'></p>
+                    </div>
+                    <div className='formElement'>
+                        <label htmlFor="message">Message</label>
+                        <textarea id="message" ref={refMessage} name="message" placeholder='How can I help?' />
+                        <p ref={refMessageError} className='formErrorMsg'></p>
+                    </div>
+                    <StandardButton value='SEND MESSAGE' mode='darkButton' />
                 </div>
-                <div className='formElement'>
-                    <label htmlFor="email">Email Address</label>
-                    <input type="email" ref={refEmail} id="email" name="email" required placeholder='email@example.com'/>
-                    <p ref={refEmailError} className='formErrorMsg'></p>
+            </form>
+            <div ref={refConfirmationWindow} id="contactConfirmationWindow" style={{display:'none'}}>
+                <div id="contactSentMessage">
+                    <p id="contactSentMessageHead">Thank you !</p>
+                    <p id="contactSentMessageText">  I’ve received your message and will respond as soon as possible.</p>
+                    <StandardButton value='Accept' mode='darkButton' handleClick={hideConfirmationWindow}/>
                 </div>
-                <div className='formElement'>
-                    <label htmlFor="message">Message</label>
-                    <textarea id="message" ref={refMessage} name="message" required placeholder='How can I help?'/>
-                    <p ref={refMessageError} className='formErrorMsg'></p>
-                </div>
-                <StandardButton value='SEND MESSAGE' mode='darkButton' handleClick={sendForm}/>
             </div>
         </section>
     )
