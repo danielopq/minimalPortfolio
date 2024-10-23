e73d9cbfc0821a2081b739106b8cfee8e3dc9121
Daniel Martinez Duque <danielopq@gmail.com>
2024-10-23 11:37:53 +0100

    ContactMe.tsx - comments added

--- src/components/contactMe/ContactMe.tsx
+++ src/components/contactMe/ContactMe.tsx
@@ -1,7 +1,10 @@
-import './contactMe.css';
 import GetInTouch from './getInTouch/GetInTouch';
 import ContactForm from './contactForm/ContactForm';
 
+/**
+ * Renders the 'Contact me' section where the user can send a contact message 
+ * @returns {JSX.Element} - The ContactMe component.
+ */
 const ContactMe: React.FC = () => {
     return (
         <main id="contactMe">
