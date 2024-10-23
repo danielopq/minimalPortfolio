6100854d25b2cd6301f5765895c808bdb32661ce
Daniel Martinez Duque <danielopq@gmail.com>
2024-10-23 13:29:32 +0100

    DetailsView.tsx - tablet version done

--- src/components/details/detailsView/DetailsView.tsx
+++ src/components/details/detailsView/DetailsView.tsx
@@ -1,7 +1,7 @@
 import './detailsView.css';
 import StandardButton from '../../shared/standardButton/StandardButton';
 import portfolioData, { Project } from '../../shared/portfolioData/portfolioData';
-import {loadWebsite} from '../../utils/utils';
+import { loadWebsite } from '../../utils/utils';
 
 interface DetailsViewProps {
     projectIndex: number;
@@ -22,20 +22,20 @@ const DetailsView: React.FC<DetailsViewProps> = ({ projectIndex }) => {
             <div id="detailsView-mainPic" style={{ backgroundImage: `url("/img/${title}/details-mainPic.jpg")` }}></div>
             <div id="detailsView-content">
                 <div id="detailsView-overview">
-                    <div>
-                        <h2>{title}</h2>
-                        <p className='detailsOverviewText'>{overviewText}</p>
+                    <h2 id="detailsView-overview-title">{title}</h2>
+                    <p id="detailsView-overview-text" className='mainText'>{overviewText}</p>
+                    <div id="detailsView-overview-keyWords">
                         <p className='keyWords'>{developmentKeyWords}</p>
                         <p className='keyWords'>{technologyKeyWords}</p>
-                        <StandardButton value='VIEW WEBSITE' mode='lightButton' handleClick={() => loadWebsite(url)}/>
+                        <StandardButton value='VIEW WEBSITE' mode='lightButton' handleClick={() => loadWebsite(url)} />
                     </div>
                 </div>
                 <div id="detailsView-description">
                     <h3>Project Background</h3>
                     <p className='mainText'>{backgroundText}</p>
                     <h3>Static Previews</h3>
-                    <div id="previewPic01" className='previewPic' style={{ backgroundImage: `url("/img/${title}/preview01.jpg")`}}></div>
-                    <div id="previewPic02" className='previewPic' style={{ backgroundImage: `url("/img/${title}/preview02.jpg")`}}></div>
+                    <div id="previewPic01" className='previewPic' style={{ backgroundImage: `url("/img/${title}/preview01.jpg")` }}></div>
+                    <div id="previewPic02" className='previewPic' style={{ backgroundImage: `url("/img/${title}/preview02.jpg")` }}></div>
                 </div>
             </div>
         </section>
