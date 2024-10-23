73bea9d8a565a3492a3708eb6f99bba7cfa570c5
Daniel Martinez Duque <danielopq@gmail.com>
2024-10-23 15:41:13 +0100

    NavBar.tsx - mobile menu container and icons created

--- src/components/shared/navBar/NavBar.tsx
+++ src/components/shared/navBar/NavBar.tsx
@@ -1,9 +1,9 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './navBar.css';
 
 interface NavbarProps {
-    selectedSection:string;
-    changeSection:(section:string)=>void;
+    selectedSection: string;
+    changeSection: (section: string) => void;
 }
 
 /**
@@ -13,13 +13,15 @@ interface NavbarProps {
  * @param {function} param0.changeSection - Function to navigate to the specified section of the website.
  * @returns {JSX.Element} The main navigation bar component.
  */
-const Navbar: React.FC<NavbarProps> = ({selectedSection='home',changeSection}) => {
+const Navbar: React.FC<NavbarProps> = ({ selectedSection = 'home', changeSection }) => {
 
     const [currentSection, setCurrentSection] = useState<string>(selectedSection);
+    const refIcon = useRef<HTMLButtonElement>(null);
+    const refMobileMenu = useRef<HTMLDivElement>(null);
 
-    useEffect(()=>{
+    useEffect(() => {
         changeMenu(selectedSection)
-    },[selectedSection]);
+    }, [selectedSection]);
 
     /**
      * Updates the current section state to the selected section.
@@ -29,14 +31,34 @@ const Navbar: React.FC<NavbarProps> = ({selectedSection='home',changeSection}) =
         section !== 'details' && setCurrentSection(section);
     }
 
+    const displayMobileMenu = (): void => {
+        if (refIcon.current && refMobileMenu.current) {
+            if(refIcon.current.className === 'mobileMenuIcon-burger'){
+                refIcon.current.className = 'mobileMenuIcon-close';
+                refMobileMenu.current.className='showNavBarMobileMenu';
+                document.body.style.position = 'fixed';
+            }else{
+                refIcon.current.className = 'mobileMenuIcon-burger';
+                refMobileMenu.current.className='hideNavBarMobileMenu';
+                document.body.style.position = 'relative';
+            }
+        }
+    }
+
     return (
-        <nav id="navBar">
-            <div id="navBarLinks">
-                <button onClick={() => changeSection('home')} className={currentSection === 'home' ? 'navBarActiveLink' : 'navBarInactiveLink'}>HOME</button>
-                <button onClick={() => changeSection('portfolio')} className={currentSection === 'portfolio' ? 'navBarActiveLink' : 'navBarInactiveLink'}>PORTFOLIO</button>
-                <button onClick={() => changeSection('contact')} className={currentSection === 'contact' ? 'navBarActiveLink' : 'navBarInactiveLink'}>CONTACT ME</button>
-            </div>
-        </nav>
+        <>
+            <nav id="navBar">
+                <div id="navBarLinks">
+                    <button onClick={() => changeSection('home')} className={currentSection === 'home' ? 'navBarActiveLink' : 'navBarInactiveLink'}>HOME</button>
+                    <button onClick={() => changeSection('portfolio')} className={currentSection === 'portfolio' ? 'navBarActiveLink' : 'navBarInactiveLink'}>PORTFOLIO</button>
+                    <button onClick={() => changeSection('contact')} className={currentSection === 'contact' ? 'navBarActiveLink' : 'navBarInactiveLink'}>CONTACT ME</button>
+                </div>
+                <button ref={refIcon} id="navBar-mobileMenuIcon" className='mobileMenuIcon-burger' onClick={displayMobileMenu}></button>
+            </nav>
+            <menu ref={refMobileMenu} id="navBarMobileMenu" className='hideNavBarMobileMenu'>
+
+            </menu>
+        </>
     )
 }
 export default Navbar;
