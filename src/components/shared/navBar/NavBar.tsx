64633fd6de244e3a8d200ca8326c5856ee9526b0
Daniel Martinez Duque <danielopq@gmail.com>
2024-10-24 13:43:59 +0100

    NavBar.tsx - mobile version done

--- src/components/shared/navBar/NavBar.tsx
+++ src/components/shared/navBar/NavBar.tsx
@@ -17,47 +17,68 @@ const Navbar: React.FC<NavbarProps> = ({ selectedSection = 'home', changeSection
 
     const [currentSection, setCurrentSection] = useState<string>(selectedSection);
     const refIcon = useRef<HTMLButtonElement>(null);
-    const refMobileMenu = useRef<HTMLDivElement>(null);
+    const refMobileNavBar = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
         changeMenu(selectedSection)
     }, [selectedSection]);
 
     /**
-     * Updates the current section state to the selected section.
-     * @param {string} section - The section to navigate to.
+     * Updates the current section state to the given section.
+     * 
+     * @param {string} section - The section to set as the current active section.
      */
     const changeMenu = (section: string) => {
         section !== 'details' && setCurrentSection(section);
     }
 
-    const displayMobileMenu = (): void => {
-        if (refIcon.current && refMobileMenu.current) {
-            if(refIcon.current.className === 'mobileMenuIcon-burger'){
+    /**
+     * Toggles the visibility of the mobile navigation bar, updates the icon's appearance, 
+     * and controls the scroll behavior of the website.
+     */
+    const displayMobileNavBar = (): void => {
+        if (refIcon.current && refMobileNavBar.current) {
+            if (refIcon.current.className === 'mobileMenuIcon-burger') {
                 refIcon.current.className = 'mobileMenuIcon-close';
-                refMobileMenu.current.className='showNavBarMobileMenu';
+                refMobileNavBar.current.className = 'showMobileNavBar';
                 document.body.style.position = 'fixed';
-            }else{
+            } else {
                 refIcon.current.className = 'mobileMenuIcon-burger';
-                refMobileMenu.current.className='hideNavBarMobileMenu';
+                refMobileNavBar.current.className = 'hideMobileNavBar';
                 document.body.style.position = 'relative';
             }
         }
     }
 
+    /**
+     * Handles the click event for navigating to a different section.
+     * 
+     * @param {string} section - The section to navigate to.
+     */
+    const handleClick = (section: string):void=>{
+        changeSection(section);
+        if(refMobileNavBar.current){
+            refMobileNavBar.current.className !== 'hideMobileNavBar' &&  displayMobileNavBar();
+        }
+    }
+
     return (
         <>
-            <nav id="navBar">
-                <div id="navBarLinks">
-                    <button onClick={() => changeSection('home')} className={currentSection === 'home' ? 'navBarActiveLink' : 'navBarInactiveLink'}>HOME</button>
-                    <button onClick={() => changeSection('portfolio')} className={currentSection === 'portfolio' ? 'navBarActiveLink' : 'navBarInactiveLink'}>PORTFOLIO</button>
-                    <button onClick={() => changeSection('contact')} className={currentSection === 'contact' ? 'navBarActiveLink' : 'navBarInactiveLink'}>CONTACT ME</button>
-                </div>
-                <button ref={refIcon} id="navBar-mobileMenuIcon" className='mobileMenuIcon-burger' onClick={displayMobileMenu}></button>
+            <nav id="desktopNavBar">
+                <menu id="desktopMenu" className="navBarMenu">
+                    <button onClick={() => handleClick('home')} className={currentSection === 'home' ? 'navBarActiveLink' : 'navBarInactiveLink'}>HOME</button>
+                    <button onClick={() => handleClick('portfolio')} className={currentSection === 'portfolio' ? 'navBarActiveLink' : 'navBarInactiveLink'}>PORTFOLIO</button>
+                    <button onClick={() => handleClick('contact')} className={currentSection === 'contact' ? 'navBarActiveLink' : 'navBarInactiveLink'}>CONTACT ME</button>
+                </menu>
+                <button ref={refIcon} id="navBar-mobileMenuIcon" className='mobileMenuIcon-burger' onClick={displayMobileNavBar}></button>
             </nav>
-            <menu ref={refMobileMenu} id="navBarMobileMenu" className='hideNavBarMobileMenu'>
-
-            </menu>
+            <nav ref={refMobileNavBar} id="mobileNavBar" className='hideMobileNavBar'>
+                <menu id="mobileMenu" className="navBarMenu">
+                    <button onClick={() => handleClick('home')} className={currentSection === 'home' ? 'navBarActiveLink' : 'navBarInactiveLink'}>HOME</button>
+                    <button onClick={() => handleClick('portfolio')} className={currentSection === 'portfolio' ? 'navBarActiveLink' : 'navBarInactiveLink'}>PORTFOLIO</button>
+                    <button onClick={() => handleClick('contact')} className={currentSection === 'contact' ? 'navBarActiveLink' : 'navBarInactiveLink'}>CONTACT ME</button>
+                </menu>
+            </nav>
         </>
     )
 }
